8570e988a4c59ed2fb6e1400d2e36cd91f888b94
cameronclass <kamrankutumov@gmail.com>
2024-02-01 01:09:20 +0300

    initial commit

--- src/assets/js/common/main.js
+++ src/assets/js/common/main.js
@@ -52,4 +52,40 @@ document.addEventListener("DOMContentLoaded", function () {
     alert("Text copied: " + textToCopy.textContent);
   });
 
+  (function () {
+    // future date for the countdown
+    const countDownDate = new Date(2024, 3, 24, 10).getTime();
+    const timerInfo = document.querySelector(".timer__info");
+    const daysContainer = document.querySelector(".timer__day");
+    const hoursContainer = document.querySelector(".timer__hours");
+    const minsContainer = document.querySelector(".timer__minutes");
+    const secsContainer = document.querySelector(".timer__seconds");
+    const messageContainer = document.querySelector(".timer__message");
+
+    // updating the countdown
+    const timer = setInterval(() => {
+      // today date and time
+      const now = new Date().getTime();
+      // distance between now an the count down date
+      const distance = countDownDate - now;
+      // calculate days, hours, mins, and secs
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const secs = Math.floor((distance % (1000 * 60)) / 1000);
+      // insert the datetime data
+      daysContainer.innerHTML = days + '<h3 class="timer__label">Дней</h3>';
+      hoursContainer.innerHTML = hours + '<h3 class="timer__label">Часов</h3>';
+      minsContainer.innerHTML = mins + '<h3 class="timer__label">Минут</h3>';
+      secsContainer.innerHTML = secs + '<h3 class="timer__label">Секунд</h3>';
+      // what happens when countdown ends
+      if (distance < 0) {
+        clearInterval(timer);
+        timerInfo.classList.add("is-hidden");
+        messageContainer.classList.add("is-visible");
+      }
+    }, 1000);
+  })();
 });
