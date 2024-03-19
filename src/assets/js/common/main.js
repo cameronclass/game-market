26a00dbde25ef05e41158eb8ecb520a0ed189f2c
cameronclass <kamrankutumov@gmail.com>
2024-03-19 13:58:39 +0300

    initial commit

--- src/assets/js/common/main.js
+++ src/assets/js/common/main.js
@@ -99,9 +99,10 @@ hamburgerButton.addEventListener("click", function () {
 const langButton = document.querySelector(".header__lang_button");
 const langDropdown = document.querySelector(".header__lang_dropdown");
 
-langButton.addEventListener("click", function () {
-  langDropdown.classList.toggle("active");
-});
+if (langButton)
+  langButton.addEventListener("click", function () {
+    langDropdown.classList.toggle("active");
+  });
 
 // Init JS
 const ReadSmore = window.readSmore;
@@ -110,30 +111,27 @@ const readMoreEls = document.querySelectorAll(".js-read-smore");
 
 ReadSmore(readMoreEls).init();
 
-/* Select */
-var x, i, j, l, ll, selElmnt, a, b, c;
-/*look for any elements with the class "custom-select":*/
+/* var x, i, j, l, ll, selElmnt, a, b, c;
+
 x = document.getElementsByClassName("custom-select");
 l = x.length;
 for (i = 0; i < l; i++) {
   selElmnt = x[i].getElementsByTagName("select")[0];
   ll = selElmnt.length;
-  /*for each element, create a new DIV that will act as the selected item:*/
+
   a = document.createElement("DIV");
   a.setAttribute("class", "select-selected");
   a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
   x[i].appendChild(a);
-  /*for each element, create a new DIV that will contain the option list:*/
+
   b = document.createElement("DIV");
   b.setAttribute("class", "select-items select-hide");
   for (j = 1; j < ll; j++) {
-    /*for each option in the original select element,
-    create a new DIV that will act as an option item:*/
+
     c = document.createElement("DIV");
     c.innerHTML = selElmnt.options[j].innerHTML;
     c.addEventListener("click", function (e) {
-      /*when an item is clicked, update the original select box,
-        and the selected item:*/
+
       var y, i, k, s, h, sl, yl;
       s = this.parentNode.parentNode.getElementsByTagName("select")[0];
       sl = s.length;
@@ -157,8 +155,6 @@ for (i = 0; i < l; i++) {
   }
   x[i].appendChild(b);
   a.addEventListener("click", function (e) {
-    /*when the select box is clicked, close any other select boxes,
-      and open/close the current select box:*/
     e.stopPropagation();
     closeAllSelect(this);
     this.nextSibling.classList.toggle("select-hide");
@@ -166,8 +162,7 @@ for (i = 0; i < l; i++) {
   });
 }
 function closeAllSelect(elmnt) {
-  /*a function that will close all select boxes in the document,
-  except the current select box:*/
+
   var x,
     y,
     i,
@@ -191,6 +186,93 @@ function closeAllSelect(elmnt) {
     }
   }
 }
-/*if the user clicks anywhere outside the select box,
-then close all select boxes:*/
-document.addEventListener("click", closeAllSelect);
+
+document.addEventListener("click", closeAllSelect); */
+
+// Функция для создания кастомных селектов
+function createCustomSelects() {
+  const customSelects = document.querySelectorAll(".custom-select");
+  customSelects.forEach((customSelect) => {
+    const selectElement = customSelect.querySelector("select");
+    createSelectContainer(customSelect, selectElement);
+    createOptionsList(customSelect, selectElement);
+    addSelectEventListeners(customSelect);
+  });
+}
+
+// Функция для создания контейнера выбранного элемента
+function createSelectContainer(customSelect, selectElement) {
+  const selectedContainer = document.createElement("DIV");
+  selectedContainer.classList.add("select-selected");
+  selectedContainer.innerHTML =
+    selectElement.options[selectElement.selectedIndex].innerHTML;
+  customSelect.appendChild(selectedContainer);
+}
+
+// Функция для создания списка опций
+function createOptionsList(customSelect, selectElement) {
+  const optionsList = document.createElement("DIV");
+  optionsList.classList.add("select-items", "select-hide");
+  for (let i = 1; i < selectElement.length; i++) {
+    const optionItem = document.createElement("DIV");
+    optionItem.innerHTML = selectElement.options[i].innerHTML;
+    optionItem.addEventListener("click", function (event) {
+      updateSelectBox(this);
+    });
+    optionsList.appendChild(optionItem);
+  }
+  customSelect.appendChild(optionsList);
+}
+
+// Функция для обновления выбранного элемента и селекта
+function updateSelectBox(selectedItem) {
+  const selectBox = selectedItem.parentNode.parentNode.querySelector("select");
+  const selectedContainer = selectedItem.parentNode.previousSibling;
+  selectBox.selectedIndex = Array.from(selectedBox.options).findIndex(
+    (option) => option.innerHTML === selectedItem.innerHTML
+  );
+  selectedContainer.innerHTML = selectedItem.innerHTML;
+  const sameAsSelected =
+    selectedItem.parentNode.querySelectorAll(".same-as-selected");
+  sameAsSelected.forEach((item) => item.classList.remove("same-as-selected"));
+  selectedItem.classList.add("same-as-selected");
+  selectedContainer.click();
+}
+
+// Функция для добавления обработчиков событий
+function addSelectEventListeners(customSelect) {
+  const selectedContainer = customSelect.querySelector(".select-selected");
+  selectedContainer.addEventListener("click", function (event) {
+    event.stopPropagation();
+    closeAllSelects(this);
+    const optionsList = this.nextSibling;
+    optionsList.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+// Функция для закрытия всех селектов
+function closeAllSelects(currentSelect) {
+  const allOptionsLists = document.querySelectorAll(".select-items");
+  const allSelectedContainers = document.querySelectorAll(".select-selected");
+  allOptionsLists.forEach((optionsList) => {
+    if (optionsList.previousSibling !== currentSelect) {
+      optionsList.classList.add("select-hide");
+    }
+  });
+  allSelectedContainers.forEach((selectedContainer) => {
+    if (selectedContainer !== currentSelect) {
+      selectedContainer.classList.remove("select-arrow-active");
+    }
+  });
+}
+
+// Добавляем обработчик события для закрытия всех селектов при клике в любом месте документа
+document.addEventListener("click", function () {
+  closeAllSelects(null);
+});
+
+createCustomSelects();
+
+/* // Вызываем функцию для создания кастомных селектов при загрузке документа
+document.addEventListener("DOMContentLoaded", createCustomSelects); */
