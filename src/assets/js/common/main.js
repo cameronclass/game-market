3b2631d83011fb5c09e4b60f385311b1f9373008
cameronclass <kamrankutumov@gmail.com>
2024-03-25 11:35:56 +0300

    initial commit

--- src/assets/js/common/main.js
+++ src/assets/js/common/main.js
@@ -26,28 +26,6 @@ accordions.forEach((accordion) => {
 
 /* Копия не работает пока */
 let copyBtn = document.querySelector(".copy-js");
-/* copyBtn.addEventListener("click", function () {
-    var textToCopy = document.querySelector(".copy-block__text");
-
-    // Create a temporary input element
-    var tempInput = document.createElement("input");
-    tempInput.setAttribute("value", textToCopy.textContent);
-
-    // Append the input to the document
-    document.body.appendChild(tempInput);
-
-    // Select the text in the input
-    tempInput.select();
-
-    // Copy the text to the clipboard
-    document.execCommand("copy");
-
-    // Remove the temporary input
-    document.body.removeChild(tempInput);
-
-    // You can add any additional feedback or actions here
-    alert("Text copied: " + textToCopy.textContent);
-  }); */
 
 /* Таймер */
 (function () {
@@ -111,85 +89,6 @@ const readMoreEls = document.querySelectorAll(".js-read-smore");
 
 ReadSmore(readMoreEls).init();
 
-/* var x, i, j, l, ll, selElmnt, a, b, c;
-
-x = document.getElementsByClassName("custom-select");
-l = x.length;
-for (i = 0; i < l; i++) {
-  selElmnt = x[i].getElementsByTagName("select")[0];
-  ll = selElmnt.length;
-
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
-
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 1; j < ll; j++) {
-
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function (e) {
-
-      var y, i, k, s, h, sl, yl;
-      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-      sl = s.length;
-      h = this.parentNode.previousSibling;
-      for (i = 0; i < sl; i++) {
-        if (s.options[i].innerHTML == this.innerHTML) {
-          s.selectedIndex = i;
-          h.innerHTML = this.innerHTML;
-          y = this.parentNode.getElementsByClassName("same-as-selected");
-          yl = y.length;
-          for (k = 0; k < yl; k++) {
-            y[k].removeAttribute("class");
-          }
-          this.setAttribute("class", "same-as-selected");
-          break;
-        }
-      }
-      h.click();
-    });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function (e) {
-    e.stopPropagation();
-    closeAllSelect(this);
-    this.nextSibling.classList.toggle("select-hide");
-    this.classList.toggle("select-arrow-active");
-  });
-}
-function closeAllSelect(elmnt) {
-
-  var x,
-    y,
-    i,
-    xl,
-    yl,
-    arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  xl = x.length;
-  yl = y.length;
-  for (i = 0; i < yl; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i);
-    } else {
-      y[i].classList.remove("select-arrow-active");
-    }
-  }
-  for (i = 0; i < xl; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
-}
-
-document.addEventListener("click", closeAllSelect); */
-
-// Функция для создания кастомных селектов
 function createCustomSelects() {
   const customSelects = document.querySelectorAll(".custom-select");
   customSelects.forEach((customSelect) => {
@@ -274,5 +173,29 @@ document.addEventListener("click", function () {
 
 createCustomSelects();
 
-/* // Вызываем функцию для создания кастомных селектов при загрузке документа
-document.addEventListener("DOMContentLoaded", createCustomSelects); */
+
+const tabButtons = document.querySelectorAll(".tab-btn");
+
+tabButtons.forEach((tab) => {
+  tab.addEventListener("click", () => tabClicked(tab));
+});
+
+function tabClicked(tab) {
+  tabButtons.forEach((tab) => {
+    tab.classList.remove("_active");
+  });
+  tab.classList.add("_active");
+
+  const contents = document.querySelectorAll(".tab-content");
+
+  contents.forEach((content) => {
+    content.classList.remove("_active");
+  });
+
+  const contentId = tab.getAttribute("content-id");
+  const contentSelected = document.getElementById(contentId);
+
+  contentSelected.classList.add("_active");
+  //console.log(contentId)
+}
+
