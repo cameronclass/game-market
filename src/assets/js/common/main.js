f7ffba89d368fcfa85923837d7328bd230f1e80b
cameronclass <kamrankutumov@gmail.com>
2024-05-04 12:31:33 +1000

    initial commit

--- src/assets/js/common/main.js
+++ src/assets/js/common/main.js
@@ -245,3 +245,10 @@ document.addEventListener("click", function (event) {
     bg.classList.remove("active");
   });
 });
+
+const scrollToTop = document.querySelector(".footer-to-top");
+
+if (scrollToTop)
+  scrollToTop.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
